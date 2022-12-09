0b4c7468a0e5346dba286a15532977dc4b4d388f
unknown <arnoldutama3@gmail.com>
2022-12-09 14:53:33 +0700

    update loginn

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -34,7 +34,7 @@
             <div class="block py-3">
                 <p >Don't have an account?</p>
                 <div class="flex ">
-                    <p>Click Here to:</p>
+                    <p>Click Here tooooo</p>
                     <a href="/register" class="underline pl-1">Register!</a>
                 </div>
             </div>
