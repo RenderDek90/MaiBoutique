7b1ca155d57253053b02fc7948fa36b9dd9156d4
arnold-utama <arnoldutama3@gmail.com>
2022-12-30 18:31:58 +0700

    seeder jadi 1

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -42,5 +42,24 @@ class DatabaseSeeder extends Seeder
         ];
 
         DB::table('users')->insert($users);
+
+        $temp_data = [
+            [
+                'image' => 'durian.png',
+                'name' => 'Durian',
+                'description' => 'Durian (Durio zibethinus) is a tropical fruit grown in Southeast Asia and highly appreciated by consumers throughout Asia. Folates are a group of vitamins and are essential nutrients for humans',
+                'price' => 20000,
+                'stock' => 20
+            ],
+            [
+                'image' => 'asd.png',
+                'name' => 'Trash',
+                'description' => 'Trash damage or destroy something, either deliberately or because you did not take good care of it',
+                'price' => 50000,
+                'stock' => 90
+            ]
+        ];
+
+        DB::table('items')->insert($temp_data);
     }
 }
