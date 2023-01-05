fd8b91a0f0738f5dd34567e208d5bdd26f08c349
RenderDek90 <74902041+RenderDek90@users.noreply.github.com>
2023-01-05 17:16:16 +0700

    Update Home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,10 +6,10 @@
         <p class="text-center text-3xl font-medium p-10">Find your best Clothes Here!!</p>
         <section class="flex flex-wrap p-10 justify-around">
             @foreach ($item as $i)
-                <div class="bg-gray-100 block leading-loose p-3 m-5 shadow-lg shadow-gray-500">
+                <div class="max-w-[20%] bg-gray-100 rounded-xl block leading-loose p-3 m-5 shadow-lg shadow-gray-500 hover:scale-105 ease-in-out duration-300 hover:shadow-gray-900">
                     <img src="{{ Storage::url('images/' . $i->image) }}" class="h-[200px] w-[200px]"alt="">
                     <p class="font-bold">{{ $i->name }}</p>
-                    <p class="py-2">Rp.{{ $i->price }}</p>
+                    <p class="py-2">Rp. {{ $i->price }}</p>
                     <a href="/item/{{ $i->id }}" class="bg-blue-500 text-white p-2 rounded">More Detail</a>
                 </div>
             @endforeach
